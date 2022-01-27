486481cd74273f7cc874a3fd26c874b4d9273fb2
unknown <you@example.com>
2022-01-27 17:43:30 +0530

    initial version

---
+++ src/main/java/com/example/demo/BootCamp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCamp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCamp1Application.class, args);
+	}
+
+}
